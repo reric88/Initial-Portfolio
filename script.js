9524515e1722de8c1c0c3ea14b93d18d66bc1479
Eric Collard <ruffyreborn@gmail.com>
2023-03-14 20:47:59 -0500

    successfully changes checkbox switches to radios, menus open on click and close on click-away

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function show1(){
+    document.getElementById('about-p').style.display ='block';
+    document.getElementById('contact-p').style.display ='none';
+    // document.getElementById('about-p').style.display ='block';
+  }
+
+  function show2(){
+    document.getElementById('about-p').style.display ='none';
+    document.getElementById('contact-p').style.display = 'block';
+  }
+
