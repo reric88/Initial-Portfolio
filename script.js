3a48e0fbbd797dbe4847f004b28ae9d158fd262f
Eric Collard <ruffyreborn@gmail.com>
2023-03-16 13:53:48 -0500

    Added additional styling to about tabs in css

--- script.js
+++ script.js
@@ -3,11 +3,19 @@
 function show1(){
     document.getElementById('about-p').style.display ='block';
     document.getElementById('contact-p').style.display ='none';
-    // document.getElementById('about-p').style.display ='block';
+    document.getElementById('placeholder-p').style.display ='none';
   }
 
   function show2(){
     document.getElementById('about-p').style.display ='none';
     document.getElementById('contact-p').style.display = 'block';
+    document.getElementById('placeholder-p').style.display ='none';
+  }
+
+  function show3(){
+    document.getElementById('about-p').style.display ='none';
+    document.getElementById('contact-p').style.display ='none';
+    document.getElementById('placeholder-p').style.display ='block';
+
   }
 
