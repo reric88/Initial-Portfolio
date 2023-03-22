f1f7aa86bcdae144ded9246652ba05eebc73c451
Eric Collard <ruffyreborn@gmail.com>
2023-03-22 09:49:59 -0500

    reworking navbar

---
+++ Bootstrap/script.js
@@ -0,0 +1,5 @@
+// let menuBtn = document.querySelector('.menu-btn');
+// let nav = document.querySelector('nav');
+// menuBtn.addEventListener('click', function() {
+//   nav.classList.toggle('show-menu');
+// });
--- script.js
+++ script.js
@@ -47,3 +47,45 @@ function typingText() {
       setTimeout(placeType, speed);
     }
   }
+
+
+  (function() {
+    // Bind Click event to the drop down navigation button
+    document.querySelector('.nav-button').addEventListener('click', function() {
+      /*  Toggle the CSS closed class which reduces the height of the UL thus 
+          hiding all LI apart from the first */
+      this.parentNode.parentNode.classList.toggle('closed')
+    }, false);
+  })();
+
+
+
+
+
+
+
+
+
+
+
+   // search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+searchBox.addEventListener("click", ()=>{
+  navbar.classList.toggle("showInput");
+  if(navbar.classList.contains("showInput")){
+    searchBox.classList.replace("bx-search" ,"bx-x");
+  }else {
+    searchBox.classList.replace("bx-x" ,"bx-search");
+  }
+});
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
